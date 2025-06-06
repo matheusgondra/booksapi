855a50f79878d2221fd1997f33a0fe6330458132
matheusgondra <matheusgondra951@gmail.com>
2025-06-06 07:53:28 -0300

    test: ensure SignUpService throw UserAlreadyExistsException if user already exists

--- src/test/java/com/matheusgondra/booksapi/application/service/SignUpServiceTest.java
+++ src/test/java/com/matheusgondra/booksapi/application/service/SignUpServiceTest.java
@@ -1,6 +1,11 @@
 package com.matheusgondra.booksapi.application.service;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +15,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.matheusgondra.booksapi.application.protocol.gateway.LoadUserByEmailGateway;
+import com.matheusgondra.booksapi.domain.exception.UserAlreadyExistsException;
+import com.matheusgondra.booksapi.domain.models.User;
 import com.matheusgondra.booksapi.domain.usecase.SignUpUseCase.SignUpParam;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,6 +33,14 @@ class SignUpServiceTest {
         "anyLastName",
         emailMock,
         "anyPassword");
+    private final User userMock = new User(
+        UUID.randomUUID(),
+        signupParamMock.firstName(),
+        signupParamMock.lastName(),
+        signupParamMock.email(),
+        signupParamMock.password(),
+        LocalDateTime.now(),
+        LocalDateTime.now());
 
     @Test
     @DisplayName("should call loadUserByEmailGateway with correct param")
@@ -33,5 +48,13 @@ class SignUpServiceTest {
         sut.signUp(signupParamMock);
 
         verify(loadUserByEmailGateway).loadByEmail(emailMock);
+    }
+
+    @Test
+    @DisplayName("Should throw UserAlreadyExistsException if user already exists")
+    void case02() {
+        when(loadUserByEmailGateway.loadByEmail(emailMock)).thenReturn(userMock);
+
+        assertThrows(UserAlreadyExistsException.class, () -> sut.signUp(signupParamMock));
     }
 }

edb6718db64ba909927fb2c799ec2e1fa207864a
matheusgondra <matheusgondra951@gmail.com>
2025-06-07 11:08:27 -0300

    test: ensure UserGateway return a Optional User if Repository.findByEmail returns a User

--- src/test/java/com/matheusgondra/booksapi/infrastructure/gateway/UserGatewayTest.java
+++ src/test/java/com/matheusgondra/booksapi/infrastructure/gateway/UserGatewayTest.java
@@ -1,9 +1,14 @@
 package com.matheusgondra.booksapi.infrastructure.gateway;
 
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.UUID;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -13,6 +18,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.matheusgondra.booksapi.domain.models.User;
+import com.matheusgondra.booksapi.infrastructure.entity.UserEntity;
 import com.matheusgondra.booksapi.infrastructure.repository.UserRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,6 +32,19 @@ public class UserGatewayTest {
 	@Nested
 	class LoadUserByEmailGatewayTests {
 		private final String email = "any@email.com";
+		private final UserEntity userEntityMock = new UserEntity(
+				UUID.randomUUID(),
+				"anyFirstName",
+				"anyLastName",
+				email,
+				"anyPassword",
+				LocalDateTime.now(),
+				LocalDateTime.now());
+
+		@BeforeEach
+		void setup() {
+			lenient().when(userRepository.findByEmail(email)).thenReturn(Optional.of(userEntityMock));
+		}
 
 		@Test
 		@DisplayName("Should call UserRepository.findByEmail with correct valid")
@@ -38,9 +57,27 @@ public class UserGatewayTest {
 		@Test
 		@DisplayName("Should return empty Optional if UserRepository.findByEmail returns null")
 		void case02() {
+			when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
+
 			Optional<User> result = sut.loadByEmail(email);
 
 			assert result.isEmpty();
+		}
+
+		@Test
+		@DisplayName("Should return Optional User if UserRepository.findByEmail returns a User")
+		void case03() {
+			Optional<User> result = sut.loadByEmail(email);
+			User user = result.get();
+
+			assert result.isPresent();
+			assert user.getId().equals(userEntityMock.getId());
+			assert user.getFirstName().equals(userEntityMock.getFirstName());
+			assert user.getLastName().equals(userEntityMock.getLastName());
+			assert user.getEmail().equals(userEntityMock.getEmail());
+			assert user.getPassword().equals(userEntityMock.getPassword());
+			assert user.getCreatedAt().equals(userEntityMock.getCreatedAt());
+			assert user.getUpdatedAt().equals(userEntityMock.getUpdatedAt());
 		}
 	}
 }

9e7c7f3eaa02c5defaad9c60eaf12a3097cc720e
matheusgondra <matheusgondra951@gmail.com>
2025-06-12 12:37:44 -0300

    feat: ensure LoginService call LoadUserByEmailGateway with correct value

---
+++ src/main/java/com/matheusgondra/booksapi/application/service/LoginService.java
@@ -0,0 +1,18 @@
+package com.matheusgondra.booksapi.application.service;
+
+import com.matheusgondra.booksapi.application.protocol.gateway.LoadUserByEmailGateway;
+import com.matheusgondra.booksapi.domain.usecase.LoginUseCase;
+
+public class LoginService implements LoginUseCase {
+    private final LoadUserByEmailGateway loadUserByEmailGateway;
+
+    public LoginService(LoadUserByEmailGateway loadUserByEmailGateway) {
+        this.loadUserByEmailGateway = loadUserByEmailGateway;
+    }
+
+    @Override
+    public String login(LoginRequest request) {
+        this.loadUserByEmailGateway.loadByEmail(request.email());
+        return null;
+    }
+}
---
+++ src/main/java/com/matheusgondra/booksapi/domain/usecase/LoginUseCase.java
@@ -0,0 +1,7 @@
+package com.matheusgondra.booksapi.domain.usecase;
+
+public interface LoginUseCase {
+    String login(LoginRequest request);
+
+    record LoginRequest(String email, String password) {}
+}

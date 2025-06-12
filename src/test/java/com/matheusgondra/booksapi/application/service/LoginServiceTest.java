d4967aefafe61d9e6445fff75d8aa630851ce157
matheusgondra <matheusgondra951@gmail.com>
2025-06-12 12:39:24 -0300

    test: ensure LoginService throw LoadUserByEmailGateway throws

--- src/test/java/com/matheusgondra/booksapi/application/service/LoginServiceTest.java
+++ src/test/java/com/matheusgondra/booksapi/application/service/LoginServiceTest.java
@@ -1,6 +1,8 @@
 package com.matheusgondra.booksapi.application.service;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,5 +30,13 @@ public class LoginServiceTest {
         sut.login(request);
 
         verify(loadUserByEmailGateway).loadByEmail(request.email());
+    }
+
+    @Test
+    @DisplayName("Should throw if LoadUserByEmailGateway throws")
+    void case02() {
+        when(loadUserByEmailGateway.loadByEmail(request.email())).thenThrow(new RuntimeException());
+
+        assertThrows(RuntimeException.class, () -> sut.login(request));
     }
 }

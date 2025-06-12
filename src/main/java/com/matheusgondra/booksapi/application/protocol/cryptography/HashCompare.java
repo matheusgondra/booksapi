c92cbae0e8f136b4e8989f084bf095a9d1d4ac55
matheusgondra <matheusgondra951@gmail.com>
2025-06-12 12:52:23 -0300

    feat: ensure LoginService call HashCompare with correct values

---
+++ src/main/java/com/matheusgondra/booksapi/application/protocol/cryptography/HashCompare.java
@@ -0,0 +1,5 @@
+package com.matheusgondra.booksapi.application.protocol.cryptography;
+
+public interface HashCompare {
+    boolean compare(String rawValue, String hashedValue);
+}
--- src/main/java/com/matheusgondra/booksapi/application/service/LoginService.java
+++ src/main/java/com/matheusgondra/booksapi/application/service/LoginService.java
@@ -1,19 +1,27 @@
 package com.matheusgondra.booksapi.application.service;
 
+import com.matheusgondra.booksapi.application.protocol.cryptography.HashCompare;
 import com.matheusgondra.booksapi.application.protocol.gateway.LoadUserByEmailGateway;
 import com.matheusgondra.booksapi.domain.exception.InvalidCredentialsException;
+import com.matheusgondra.booksapi.domain.models.User;
 import com.matheusgondra.booksapi.domain.usecase.LoginUseCase;
 
 public class LoginService implements LoginUseCase {
     private final LoadUserByEmailGateway loadUserByEmailGateway;
+    private final HashCompare hashCompare;
 
-    public LoginService(LoadUserByEmailGateway loadUserByEmailGateway) {
+    public LoginService(LoadUserByEmailGateway loadUserByEmailGateway, HashCompare hashCompare) {
         this.loadUserByEmailGateway = loadUserByEmailGateway;
+        this.hashCompare = hashCompare;
     }
 
     @Override
     public String login(LoginRequest request) {
-        this.loadUserByEmailGateway.loadByEmail(request.email()).orElseThrow(InvalidCredentialsException::new);
+        User user = this.loadUserByEmailGateway.loadByEmail(request.email())
+                .orElseThrow(InvalidCredentialsException::new);
+
+        this.hashCompare.compare(request.password(), user.getPassword());
+
         return null;
     }
 }

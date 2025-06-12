125ba2c36fb5c3927a1fb224cf64a40a26906195
matheusgondra <matheusgondra951@gmail.com>
2025-06-12 12:48:45 -0300

    feat: ensure LoginService throw InvalidCredentialsException if LoadUserByEmailGateway returns empty

--- src/main/java/com/matheusgondra/booksapi/application/service/LoginService.java
+++ src/main/java/com/matheusgondra/booksapi/application/service/LoginService.java
@@ -1,6 +1,7 @@
 package com.matheusgondra.booksapi.application.service;
 
 import com.matheusgondra.booksapi.application.protocol.gateway.LoadUserByEmailGateway;
+import com.matheusgondra.booksapi.domain.exception.InvalidCredentialsException;
 import com.matheusgondra.booksapi.domain.usecase.LoginUseCase;
 
 public class LoginService implements LoginUseCase {
@@ -12,7 +13,7 @@ public class LoginService implements LoginUseCase {
 
     @Override
     public String login(LoginRequest request) {
-        this.loadUserByEmailGateway.loadByEmail(request.email());
+        this.loadUserByEmailGateway.loadByEmail(request.email()).orElseThrow(InvalidCredentialsException::new);
         return null;
     }
 }
---
+++ src/main/java/com/matheusgondra/booksapi/domain/exception/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package com.matheusgondra.booksapi.domain.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException() {
+        super("Invalid credentials");
+    }
+}

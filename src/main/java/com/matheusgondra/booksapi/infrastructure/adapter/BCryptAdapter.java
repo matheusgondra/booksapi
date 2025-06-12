3438570bc3440342a51e4d18ae006126be0bc12b
matheusgondra <matheusgondra951@gmail.com>
2025-06-12 13:12:48 -0300

    feat: ensure BCryptAdapter.compare call PasswordEncoder.matches with correct values

--- src/main/java/com/matheusgondra/booksapi/infrastructure/adapter/BCryptAdapter.java
+++ src/main/java/com/matheusgondra/booksapi/infrastructure/adapter/BCryptAdapter.java
@@ -2,9 +2,10 @@ package com.matheusgondra.booksapi.infrastructure.adapter;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.matheusgondra.booksapi.application.protocol.cryptography.HashCompare;
 import com.matheusgondra.booksapi.application.protocol.cryptography.HashGenerator;
 
-public class BCryptAdapter implements HashGenerator {
+public class BCryptAdapter implements HashGenerator, HashCompare {
 	private final PasswordEncoder encoder;
 
 	public BCryptAdapter(PasswordEncoder encoder) {
@@ -14,5 +15,10 @@ public class BCryptAdapter implements HashGenerator {
 	@Override
 	public String generate(String value) {
 		return this.encoder.encode(value);
+	}
+
+	@Override
+	public boolean compare(String rawValue, String hashedValue) {
+		return this.encoder.matches(rawValue, hashedValue);
 	}
 }

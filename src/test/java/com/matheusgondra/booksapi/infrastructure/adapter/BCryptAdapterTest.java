fbf2e93e457a00c25060df72cc66087fdf30dfea
matheusgondra <matheusgondra951@gmail.com>
2025-06-12 13:12:55 -0300

    test: ensure BCryptAdapter.compare call PasswordEncoder.matches with correct values

--- src/test/java/com/matheusgondra/booksapi/infrastructure/adapter/BCryptAdapterTest.java
+++ src/test/java/com/matheusgondra/booksapi/infrastructure/adapter/BCryptAdapterTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -21,30 +22,46 @@ public class BCryptAdapterTest {
 	private PasswordEncoder encoder;
 
 	private final String value = "anyValue";
+	private final String hashedValue = "hashedValue";
 
-	@Test
-	@DisplayName("Should call PasswordEncoder.encode with correct value")
-	void case01() {
-		sut.generate(value);
+	@Nested
+	class HashGeneratorTests {
+		@Test
+		@DisplayName("Should call PasswordEncoder.encode with correct value")
+		void case01() {
+			sut.generate(value);
+	
+			verify(encoder).encode(value);
+		}
+	
+		@Test
+		@DisplayName("Should throw if PasswordEncoder.encode throws")
+		void case02() {
+			when(encoder.encode(value)).thenThrow(new RuntimeException("Error encoding"));
+	
+			assertThrows(RuntimeException.class, () -> sut.generate("anyValue"));
+		}
+	
+		@Test
+		@DisplayName("Should return a hash on success")
+		void case03() {
+			when(encoder.encode(value)).thenReturn(hashedValue);
 
-		verify(encoder).encode(value);
+			String result = sut.generate(value);
+
+			assert result.equals(hashedValue);
+		}
 	}
 
-	@Test
-	@DisplayName("Should throw if PasswordEncoder.encode throws")
-	void case02() {
-		when(encoder.encode(value)).thenThrow(new RuntimeException("Error encoding"));
 
-		assertThrows(RuntimeException.class, () -> sut.generate("anyValue"));
-	}
+	@Nested
+	class HashCompareTests {
+		@Test
+		@DisplayName("Should call PasswordEncoder.matches with correct values")
+		void case01() {
+			sut.compare(value, hashedValue);
 
-	@Test
-	@DisplayName("Should return a hash on success")
-	void case03() {
-		when(encoder.encode(value)).thenReturn("hashedValue");
-
-		String result = sut.generate(value);
-
-		assert result.equals("hashedValue");
+			verify(encoder).matches(value, hashedValue);
+		}
 	}
 }

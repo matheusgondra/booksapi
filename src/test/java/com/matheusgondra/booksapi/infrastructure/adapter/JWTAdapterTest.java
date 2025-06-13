8f8fc4ab3cc0036d51dc2577f22bb5959a678f72
matheusgondra <matheusgondra951@gmail.com>
2025-06-13 16:20:01 -0300

    test: ensure JWTAdapter set expiration time 1 day from now  when generating a token

--- src/test/java/com/matheusgondra/booksapi/infrastructure/adapter/JWTAdapterTest.java
+++ src/test/java/com/matheusgondra/booksapi/infrastructure/adapter/JWTAdapterTest.java
@@ -1,12 +1,17 @@
 package com.matheusgondra.booksapi.infrastructure.adapter;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.verify;
+
+import java.time.Instant;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -27,13 +32,28 @@ public class JWTAdapterTest {
 
     @Test
     @DisplayName("Should call JWT.create() when generating a token")
-    public void case01() {
+    void case01() {
         try (MockedStatic<JWT> mockedJWT = mockStatic(JWT.class)) {
             mockedJWT.when(JWT::create).thenReturn(mockBuilder);
 
             sut.generate(payload);
 
             mockedJWT.verify(JWT::create);
+        }
+    }
+
+    @Test
+    @DisplayName("Should set expiration time 1 day from now when generating a token")
+    void case02() {
+        try (MockedStatic<JWT> mockedJWT = mockStatic(JWT.class)) {
+            mockedJWT.when(JWT::create).thenReturn(mockBuilder);
+            mockedJWT.when(() -> mockBuilder.withExpiresAt(any(Instant.class))).thenReturn(mockBuilder);
+
+            ArgumentCaptor<Instant> captor = ArgumentCaptor.forClass(Instant.class);
+
+            sut.generate(payload);
+
+            verify(mockBuilder).withExpiresAt(captor.capture());
         }
     }
 }

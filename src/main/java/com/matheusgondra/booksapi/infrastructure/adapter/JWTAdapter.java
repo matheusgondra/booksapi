099cc2fdcd935e2f494011b39fdd6874ee7b2ded
matheusgondra <matheusgondra951@gmail.com>
2025-06-16 22:15:38 -0300

    feat: ensure JWTAdapter set expiration time 1 day from now when generating a token

--- src/main/java/com/matheusgondra/booksapi/infrastructure/adapter/JWTAdapter.java
+++ src/main/java/com/matheusgondra/booksapi/infrastructure/adapter/JWTAdapter.java
@@ -9,21 +9,21 @@ import com.auth0.jwt.JWT;
 import com.matheusgondra.booksapi.application.protocol.cryptography.TokenGenerator;
 
 public class JWTAdapter implements TokenGenerator {
-    @Value("${jwt.secret}")
-    private String secret;
+	@Value("${jwt.secret}")
+	private String secret;
 
-    public JWTAdapter(String secret) {
-        this.secret = secret;
-    }
+	public JWTAdapter(String secret) {
+		this.secret = secret;
+	}
 
-    @Override
-    public String generate(String payload) {
-        Duration oneDay = Duration.ofDays(1);
-        Instant expiresAt = Instant.now().plus(oneDay);
-        
-        JWT.create()
-                .withExpiresAt(expiresAt);
+	@Override
+	public String generate(String payload) {
+		Duration oneDay = Duration.ofDays(1);
+		Instant expiresAt = Instant.now().plus(oneDay);
 
-        return null;
-    }
+		JWT.create()
+				.withExpiresAt(expiresAt);
+
+		return null;
+	}
 }

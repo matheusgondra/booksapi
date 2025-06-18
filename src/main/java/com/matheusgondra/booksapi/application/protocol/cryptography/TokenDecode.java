f6cbd82853c95fd9226283298e49d8ca514cb591
matheusgondra <matheusgondra951@gmail.com>
2025-06-18 09:07:39 -0300

    feat: ensure JWTAdapter.decode call JWT.require() when decoding a token

---
+++ src/main/java/com/matheusgondra/booksapi/application/protocol/cryptography/TokenDecode.java
@@ -0,0 +1,5 @@
+package com.matheusgondra.booksapi.application.protocol.cryptography;
+
+public interface TokenDecode {
+    String decode(String token);
+}
--- src/main/java/com/matheusgondra/booksapi/infrastructure/adapter/JWTAdapter.java
+++ src/main/java/com/matheusgondra/booksapi/infrastructure/adapter/JWTAdapter.java
@@ -5,13 +5,14 @@ import java.time.Instant;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.matheusgondra.booksapi.application.protocol.cryptography.TokenDecode;
 import com.matheusgondra.booksapi.application.protocol.cryptography.TokenGenerator;
 
-public class JWTAdapter implements TokenGenerator {
-	private String secret;
+public class JWTAdapter implements TokenGenerator, TokenDecode {
+	private final Algorithm algorithm;
 
 	public JWTAdapter(String secret) {
-		this.secret = secret;
+		this.algorithm = Algorithm.HMAC256(secret);
 	}
 
 	@Override
@@ -19,13 +20,18 @@ public class JWTAdapter implements TokenGenerator {
 		Duration oneDay = Duration.ofDays(1);
 		Instant expiresAt = Instant.now().plus(oneDay);
 
-		Algorithm algorithm = Algorithm.HMAC256(this.secret);
-
 		String accessToken = JWT.create()
 				.withExpiresAt(expiresAt)
 				.withSubject(payload)
 				.sign(algorithm);
 
 		return accessToken;
+	}
+
+	@Override
+	public String decode(String token) {
+		JWT.require(algorithm);
+		
+		return null;
 	}
 }

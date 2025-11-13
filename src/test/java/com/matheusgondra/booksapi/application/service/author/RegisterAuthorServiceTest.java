78ec82cef870c5be4c6c2af558e2263f2c2ba27e
matheusgondra <matheusgondra951@gmail.com>
2025-11-13 07:24:48 -0300

    test: ensure RegisterAuthorService throw AuthorAlreadyExistsException when LoadAuthorByNameGateway returns an author

--- src/test/java/com/matheusgondra/booksapi/application/service/author/RegisterAuthorServiceTest.java
+++ src/test/java/com/matheusgondra/booksapi/application/service/author/RegisterAuthorServiceTest.java
@@ -1,8 +1,14 @@
 package com.matheusgondra.booksapi.application.service.author;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import com.matheusgondra.booksapi.application.protocol.gateway.LoadAuthorByNameGateway;
+import com.matheusgondra.booksapi.domain.exception.AuthorAlreadyExistsException;
+import com.matheusgondra.booksapi.domain.models.Author;
 import com.matheusgondra.booksapi.domain.usecase.author.RegisterAuthorUseCase.RegisterAuthorParam;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,6 +24,7 @@ class RegisterAuthorServiceTest {
   @Mock LoadAuthorByNameGateway loadAuthorByNameGateway;
 
   RegisterAuthorParam registerAuthorParam = new RegisterAuthorParam("anyName");
+  Author author = new Author("anyName");
 
   @Test
   @DisplayName("Should call LoadAuthorByNameGateway with correct value")
@@ -25,5 +32,13 @@ class RegisterAuthorServiceTest {
     sut.register(registerAuthorParam);
 
     verify(loadAuthorByNameGateway).loadByName(registerAuthorParam.name());
+  }
+
+  @Test
+  @DisplayName("Should throw AuthorAlreadyExistsException when LoadAuthorByNameGateway returns an author")
+  void case02() {
+    when(loadAuthorByNameGateway.loadByName("anyName")).thenReturn(Optional.of(author));
+
+    assertThrows(AuthorAlreadyExistsException.class, () -> sut.register(registerAuthorParam));
   }
 }
